3e0cf189234e611d5f77471ad53fb4ac61aa58be
sohila-mohamed <sohailamohamed.sbme@gmail.com>
2021-05-20 04:29:58 +0200

    adding header search icon with display-on/off

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
     <div>
+        <Header></Header>
         <h1>App Component</h1>
          <div class="btn-group" role="group" aria-label="Basic mixed styles example">
 </div>
     </div>
 </template>
 <script>
+import Header from './Components/Header';
 export default {
-    name:'App'
+    name:'App',
+    components:{
+        Header
+        }
 }
 </script>
 

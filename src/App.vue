3d9e22925d0684a2bd003526e4934148ed5f60bc
sohila-mohamed <sohailamohamed.sbme@gmail.com>
2021-05-20 02:01:39 +0200

    adding bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+        <h1>App Component</h1>
+         <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+</div>
+    </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name:'App'
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+h1{color: blue;}
 </style>

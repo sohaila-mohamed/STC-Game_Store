1ea0a9f31037d79f164fcbd7f7d74aa16f0cad08
sohila-mohamed <sohailamohamed.sbme@gmail.com>
2021-05-22 23:07:44 +0200

    API-Testing

--- src/Api/GamesApi.js
+++ src/Api/GamesApi.js
@@ -1,27 +1,11 @@
 import axios from 'axios';
-//import { ValidateGameObject } from '../Services/ValidationService/GameValidation'
+
+const DB_URI = 'https://my-json-server.typicode.com/sohaila-mohamed/stc-game-store/games';
 
 export default {
     FetchGamesData() {
-        return axios.get('https://my-json-server.typicode.com/sohaila-mohamed/stc-game-store/games')
+        return axios.get(DB_URI);
 
-
-        // .then(response => {
-        //         return validateData(response);
-
-        //     }).catch(err => {
-        //         //error.value = err;
-        //         console.log(err)
-        //     })
-        //     // .then(() => loading.value = false);
-
-        // function validateData(response) {
-        //     console.log("all data", response);
-        //     let filteredData = [];
-        //     for (let item of response.data) { console.log("item", item); if (ValidateGameObject(item)) filteredData.push(item); }
-        //     console.log("filtered gamed", filteredData)
-        //     return filteredData;
-        // }
     }
 
 }
